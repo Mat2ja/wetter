5ddaae3398dce20c5be19c53eafd77c70fa6d00b
Matija Osrecki <matija.osrecki@gmail.com>
2020-08-23 12:43:41 +0200

    built autocomplete :sparkles: :contruction:

--- app.js
+++ app.js
@@ -4,19 +4,39 @@ const api = {
 };
 
 const searchbox = document.querySelector('.header__search-box');
+const main = document.querySelector('main');
 const city = document.querySelector('.location__city');
 const date = document.querySelector('.location__date');
 const temperature = document.querySelector('.current__temp');
 const feel = document.querySelector('.current__feels-like');
 const weatherDescEl = document.querySelector('.current__weather');
 const highLow = document.querySelector('.current__hi-low');
+const suggestions = document.querySelector('.suggestions');
 
-searchbox.addEventListener('keypress', setQuery);
+const cities = [];
+const cityList = fetch('./city.list.json')
+    .then(blob => blob.json())
+    .then(data => {
+        for (let city of data) {
+            cities.push({ id: city.id, name: city.name, country: city.country });
+        }
+    });
 
 function setQuery(e) {
+    // Enter
     if (e.keyCode === 13) {
-        getResults(searchbox.value);
-        console.log(searchbox.value);
+        let suggestionsItems = document.querySelectorAll('.suggestions__item');
+        for (let suggestion of suggestionsItems) {
+            if (suggestion.classList.contains('active')) {
+                let city = suggestion.querySelector('.city').innerText;
+                getResults(suggestion);
+                return;
+            }
+        }
+        let city = suggestionsItems[0].querySelector('.city').innerText;
+        getResults(city);
+
+
     }
 }
 
@@ -31,12 +51,10 @@ function getResults(query) {
 }
 
 function displayResults(weather) {
-    console.log(weather);
+    clearSuggestions();
     city.innerHTML = `${weather.name}, ${weather.sys.country}`;
     // date.innerText = moment().format('dddd MMMM Do YYYY, HH:mm');
     date.innerText = moment().format('dddd D MMMM');
-
-
 
     let temp = Math.round(calculateCelsius(weather.main.temp));
     let feelsLike = Math.round(calculateCelsius(weather.main.feels_like));
@@ -62,4 +80,93 @@ function displayResults(weather) {
 
 function calculateCelsius(kelvin) {
     return kelvin - 273.15;
+}
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.name.match(regex) || place.country.match(regex);
+    })
+};
+
+function displayMatches() {
+    if (!this.value) return clearSuggestions();
+    clearWeatherData();
+
+    const matchArray = findMatches(this.value, cities);
+
+    const html = matchArray
+        .slice(0, 12)
+        .map(place => {
+            const regex = new RegExp(this.value, 'gi');
+            const cityName = place.name.replace(regex, `<span class="hl">${this.value}</span>`);
+            const countryName = place.country.replace(regex, `<span class="hl">${this.value}</span>`);
+
+            suggestions.classList.remove('hide');
+            return `
+        <li class='suggestions__item'>
+            <span class="city">${cityName}</span>
+            <span class="country">${countryName}</span>
+        </li>
+        `;
+        }).join('');
+
+    suggestions.innerHTML = html;
+
+    let suggestionsItems = document.querySelectorAll('.suggestions__item');
+
+    for (let item of suggestionsItems) {
+        item.addEventListener('click', (e) => {
+            let cityName = item.querySelector('.city').innerText;
+            getResults(cityName);
+        });
+    }
+}
+
+function clearSuggestions() {
+    suggestions.innerHTML = '';
+    counter = 0;
+
+}
+
+function clearWeatherData() {
+    city.innerText = '';
+    date.innerText = '';
+    temperature.innerText = '';
+    feel.innerText = '';
+    weatherDescEl.innerText = '';
+    highLow.innerText = '';
+}
+
+searchbox.addEventListener('input', displayMatches);
+searchbox.addEventListener('keypress', setQuery);
+
+let counter = 0;
+document.addEventListener('keyup', ({ keyCode }) => {
+    if (keyCode === 9) {
+        searchbox.focus();
+    } else if (keyCode === 40) {
+        if (suggestions.hasChildNodes()) {
+            console.log('arrow down');
+            // moveThroughSuggestions(counter++);
+        }
+    }
+});
+
+function moveThroughSuggestions(counter) {
+    // TODO
+    let suggestionsItems = document.querySelectorAll('.suggestions__item');
+    console.log('COUNTER', counter);
+
+    if (counter === suggestionsItems.length) {
+        suggestionsItems[counter - 1].classList.remove('active');
+        counter = 0;
+    }
+    if (counter === 0) {
+        suggestionsItems[counter].classList.add('active');
+        return;
+    }
+
+    suggestionsItems[counter - 1].classList.remove('active');
+    suggestionsItems[counter++].classList.add('active');
 }
